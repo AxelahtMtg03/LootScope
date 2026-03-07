39cca8c588a837cc5a487df30600f3a08ab5479d
axel <axel.archambeault@gmail.com>
2026-03-07 22:40:03 +1100

    main

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, flash,session,jsonify
+import datetime
+import infoitem
+
+app = Flask(__name__)
+title="LootScope"
+
+@app.route('/')
+def accueil():
+    current_year = datetime.datetime.now().year
+    return render_template('accueil.html',title=title,annee=current_year)
+
+@app.route('/search', methods=['POST'])
+def search():
+    if request.method == 'POST':
+        item = request.form['item']
+        x=infoitem.dropsearch(item)
+        maxchance=infoitem.maxChance(x)
+        trieChance = infoitem.trieChance(x)
+        for elem in trieChance:
+            elem["image"] = infoitem.get_place_image(elem["place"])
+        if trieChance == []:
+            return  render_template('accueil.html',title=title,message="Item pas dans la data")
+        return render_template('search.html',title=title,ls=trieChance)
+
+@app.route('/healthz')
+def health_check():
+    return "ok", 200
+# Pour Vercel, on utilise app directement
+if __name__ == '__main__':
+    app.run(debug=True)          
